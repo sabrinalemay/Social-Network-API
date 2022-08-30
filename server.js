1237fb7289705c5f6092fbe5cdf9bd5b3deed8e3
Sabrina LeMay <b33na1997@gmail.com>
2022-08-29 21:03:17 -0700

    added mongoose

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { ppid } = require("process");
 const routes = require("./routes");
 
 const app = express();
@@ -8,3 +9,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+
+mongoose.connect(
+    process.env.MODGODB_URI || "mongodb://localhost:27017/social-network-api",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+mongoose.set("debug", true);
+
+app.listen(PORT, () => console.log("connected on localhost:${PORT}"));
